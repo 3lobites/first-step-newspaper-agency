a55f9810b20c308e12177269c07cef9f478985f2
3lobites <3lobites@gmail.com>
2023-12-21 11:45:37 +0200

    creating initial tables: Topic Redactor Newspaper, edit settings AUTH_USER_MODEL

--- newspaper_agency/models.py
+++ newspaper_agency/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField
+    published_date = models.DateTimeField
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    def __str_(self):
+        return f"{self.title} ({self.topic}, redactor: {self.publishers}, publication date - {self.published_date})"
