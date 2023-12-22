49f1a894aab6e68219ad29dc1126914124f0942e
3lobites <3lobites@gmail.com>
2023-12-22 11:14:10 +0200

    Added processing of the list of news topics: viewing, adding, editing, deleting

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -1,8 +1,30 @@
 from django.urls import path
-from .views import index
+from .views import index, TopicListView, TopicCreateView, TopicUpdateView, TopicDeleteView
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "topic/",
+        TopicListView.as_view(),
+        name="topic-list",
+    ),
+    path(
+        "topic/create/",
+        TopicCreateView.as_view(),
+        name="topic-create",
+    ),
+    path(
+        "topic/<int:pk>/update/",
+        TopicUpdateView.as_view(),
+        name="topic-update",
+    ),
+    path(
+        "topic/<int:pk>/delete/",
+        TopicDeleteView.as_view(),
+        name="topic-delete",
+    ),
+
 ]
 
 app_name = "newspaper_agency"
--- newspaper_agency/views.py
+++ newspaper_agency/views.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .models import Topic
 
 
 @login_required
 def index(request):
     """View function for the home page of the site."""
 
-    # num_drivers = Driver.objects.count()
+    num_topics = Topic.objects.count()
     # num_cars = Car.objects.count()
     # num_manufacturers = Manufacturer.objects.count()
 
@@ -14,11 +19,50 @@ def index(request):
     request.session["num_visits"] = num_visits + 1
 
     context = {
-        # "num_drivers": num_drivers,
+        "num_topic": num_topics,
         # "num_cars": num_cars,
         # "num_manufacturers": num_manufacturers,
         "num_visits": num_visits + 1,
     }
-
     return render(request, "newspaper_agency/index.html", context=context)
+
+
+class TopicListView(LoginRequiredMixin, generic.ListView):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "newspaper_agency/topic_list.html"
+    paginate_by = 5
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     context = super(TopicListView, self).get_context_data(**kwargs)
+    #     name = self.request.GET.get("name", "")
+    #     context["search_form"] = TopicSearchForm(
+    #         initial={"name": name}
+    #     )
+    #     return context
+    #
+    # def get_queryset(self):
+    #     queryset = Topic.objects.all()
+    #     form = TopicSearchForm(self.request.GET)
+    #     if form.is_valid():
+    #         return queryset.filter(name__icontains=form.cleaned_data["model"])
+    #     return queryset
+
+
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper_agency:topic-list")
+
+
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("newspaper_agency:topic-list")
+
+
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("newspaper_agency:topic-list")
+
 
